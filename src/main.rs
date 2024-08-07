0ab41ce8c4f65e8e3d0cc3e8e5c02ce4b607caca
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-08-07 19:05:14 +0800

    feat: create a simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(healthcheck_handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn healthcheck_handler() -> (StatusCode, Json<Value>) {
+    (StatusCode::OK, Json(json!({"message": "OK"})))
 }
