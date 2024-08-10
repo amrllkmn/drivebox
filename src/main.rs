79c154d8bd96b62c3e29a7afda2598f32a9b1d06
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-08-10 17:04:34 +0800

    refactor: use a variable to set address for running on local

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use serde_json::{json, Value};
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(healthcheck_handler));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let addr = "0.0.0.0:8080"; // todo: change me to run without docker
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
