f1d1218acb7f8bdaab12b04834dc7540f02ffce0
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-08-11 22:05:09 +0800

    feat: add database connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 mod handlers;
 mod routes;
 
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
 #[tokio::main]
 async fn main() {
-    let app = routes::create_api_route();
+    dotenv().ok();
     let addr = "0.0.0.0:8080"; // todo: change me to run without docker
+    let database_url = env::var("DATABASE_URL").expect("Missing DATABASE_URL env");
+
+    let app_state = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
+    sqlx::migrate!()
+        .run(&app_state)
+        .await
+        .expect("Migrating went wrong");
+    let app = routes::create_api_route(app_state);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
