75037ec5bc4c0bb222d6fe664623165376f4b562
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-08-11 22:05:37 +0800

    feat: add user route

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,11 @@
 use crate::handlers;
 use axum::{routing::get, Router};
+use sqlx::{Pool, Postgres};
 
-pub fn create_api_route() -> Router {
-    Router::new().route("/healthcheck", get(handlers::healthcheck))
+pub fn create_api_route(app_state: Pool<Postgres>) -> Router {
+    let api_routes = Router::new().route("/users", get(handlers::get_users));
+    Router::new()
+        .route("/healthcheck", get(handlers::healthcheck))
+        .nest("/api/v1", api_routes)
+        .with_state(app_state)
 }
